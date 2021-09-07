ef2ab3cc22874d70cb42d02db228449c76024580
DeisiAsu <68709170+DeisiAsu@users.noreply.github.com>
2021-09-07 11:10:41 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,17 +9,20 @@ function IMC(elEvento){
     
 
     var formula = peso/(altura*altura);
-
     
+    // Medidas de un adulto.
+
    if(formula < 18.5){
         respuestas.innerHTML = '<h3> Bajo peso y tu IMC es de:  </h3> ' + (formula)
     } else if (formula >18.5 & formula< 24.9){
         respuestas.innerHTML = '<h3> Peso normal y tu IMC es de: </h3>' + (formula)
-    }else if (formula > 25 & formula< 29.9){
+    }else if (formula > 25 & formula < 29.9){
         respuestas.innerHTML = '<h3> Sobrepeso y tu IMC es de: </h3>'   + (formula)
-    }else if (formula >=30){
+    }else if (formula > 30 & formula < 39.9){
         respuestas.innerHTML = '<h3> Obsesidad y tu IMC es de: </h3>'   + (formula)
-    }
+    }else if (formula >=40){
+        respuestas.innerHTML = '<h3> Obsesidad extrema y tu IMC es de: </h3>'+ (formula)
+    }    
 }
 
 var boton = document.querySelector("#boton");
